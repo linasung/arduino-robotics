c4794c146e53e3d0a7cbfc305dde4d92b0bc4ae0
linasung <39501550+linasung@users.noreply.github.com>
2018-05-21 15:40:24 -0700

    Add files via upload

---
+++ hw1/hw1.ino
@@ -0,0 +1,49 @@
+int led = 13;
+int led2 = 8;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(500);
+  digitalWrite(led2, HIGH);
+  delay(750);
+  digitalWrite(led2, LOW);
+  delay(300);
+  digitalWrite(led2, HIGH);
+  delay(750);
+  digitalWrite(led2, LOW);
+  delay(300);
+  digitalWrite(led2, HIGH);
+  delay(750);
+  digitalWrite(led2, LOW);
+  delay(300);
+   digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(2000);
+}
+
+
+
