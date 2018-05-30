5de3ddcb73a38e2981488bf265bb1e81b27425a6
linasung <39501550+linasung@users.noreply.github.com>
2018-05-30 14:44:53 -0700

    Add files via upload

---
+++ hw3/sketch_may25a.ino
@@ -0,0 +1,67 @@
+int led = 13;
+int AIN1 = 7;
+int AIN2 = 8;
+int BIN1 = 10;
+int BIN2 = 11;
+
+void setup() {
+ pinMode(led, OUTPUT);
+ pinMode(AIN1, OUTPUT);
+ pinMode(AIN2, OUTPUT);
+ pinMode(BIN1, OUTPUT);
+ pinMode(BIN2, OUTPUT);
+}
+
+void forward() {
+ digitalWrite(led, HIGH);
+ digitalWrite(AIN1, LOW);
+ digitalWrite(AIN2, HIGH);
+ digitalWrite(BIN1, HIGH);
+ digitalWrite(BIN2, LOW);
+}
+
+void backward() {
+ digitalWrite(led, HIGH);
+ digitalWrite(AIN1, HIGH);
+ digitalWrite(AIN2, LOW);
+ digitalWrite(BIN1, LOW);
+ digitalWrite(BIN2, HIGH);
+}
+
+void stop() {
+ digitalWrite(led, LOW);
+ digitalWrite(AIN1, LOW);
+ digitalWrite(AIN2, LOW);
+ digitalWrite(BIN1, LOW);
+ digitalWrite(BIN2, LOW);
+}
+
+void left() {
+  digitalWrite(led, HIGH);
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW); 
+}
+
+void right() {
+  digitalWrite(led, HIGH);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+}
+
+void loop() {
+ forward();
+ delay(2000);
+ backward();
+ delay(2000);
+ left();
+ delay(2000);
+ right();
+ delay(2000);
+ 
+ stop();
+ delay(1000);
+}
