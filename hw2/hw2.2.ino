fb264e1457eec46f95f1e85a6ac9d948724f00be
linasung <39501550+linasung@users.noreply.github.com>
2018-05-30 14:45:39 -0700

    Add files via upload

---
+++ hw2/hw2.2.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+ Serial.begin(19200);
+ pinMode(A0, INPUT);
+ pinMode(2, OUTPUT);
+}
+void loop() {
+ int value;
+ 
+ value = analogRead(A0);
+ Serial.print(value);
+
+tone(2, value);
+delay(500);
+noTone(2);
+ 
+}
